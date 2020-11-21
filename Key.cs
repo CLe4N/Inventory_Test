745b53c84bab4864a2562105c17f27520497bec0
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 06:03:37 +0700

    Update Key.cs

--- Key.cs
+++ Key.cs
@@ -10,24 +10,28 @@ public class Key : MonoBehaviour
     public bool KeyIsShow;
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
-        KeySprite = keyItem.GetComponent<SpriteRenderer>();
+        Player = GameObject.FindGameObjectWithTag("Player"); // find gameobject with tag "Player"
+        KeySprite = keyItem.GetComponent<SpriteRenderer>(); // access SpriteRenderer component
     }
 
     public void ShowKey()
     {
-        if (KeyIsShow == false)
+        if (KeyIsShow == false) // if KeyIsShow = false
         {
             Vector2 PlayerPos = new Vector2(Player.transform.position.x, Player.transform.position.y + 1.5f);
+            // get player position
             var myKey = Instantiate(keyItem, PlayerPos, Quaternion.identity);
+            //create prefab keyItem
             myKey.transform.SetParent(Player.transform);
-            KeyIsShow = true;
+            // assige this gameobject to player as child
+            KeyIsShow = true; // KeyIsShow = true
         }
         else
         {
             GameObject Key = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).transform.gameObject;
-            Destroy(Key);
-            KeyIsShow = false;
+            // get child(0) of gameobject with tag "Player"
+            Destroy(Key); // destroy key
+            KeyIsShow = false; // KeyIsShow = false
         }
     }
 }
