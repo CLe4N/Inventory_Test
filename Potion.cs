f2cac5520eb38a2950eead3112d8d3d163be6687
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 06:15:46 +0700

    Update Potion.cs

--- Potion.cs
+++ Potion.cs
@@ -8,12 +8,12 @@ public class Potion : MonoBehaviour
     HP_Con hp;
     void Start()
     {
-        hp = GameObject.Find("HPtext").GetComponent<HP_Con>();
+        hp = GameObject.Find("HPtext").GetComponent<HP_Con>(); // find gameobject "HPtext" and access HP_Con component
     }
 
     public void Heal()
     {
-        hp.HP_Point += 1;
-        Destroy(this.gameObject);
+        hp.HP_Point += 1; // HP_Point increase by 1
+        Destroy(this.gameObject); // destroy this gameobject
     }
 }
