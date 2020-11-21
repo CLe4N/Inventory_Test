0444304d1c9a18b9319378a281d37348d1a047b3
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 06:14:18 +0700

    Update PickUp.cs

--- PickUp.cs
+++ PickUp.cs
@@ -8,21 +8,23 @@ public class PickUp : MonoBehaviour
     public GameObject itemButton;
     void Start()
     {
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>(); // Find gameobject with tag "Player" and access Inventory component
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.CompareTag("Player"))
+    {"Player"
+        if(collision.CompareTag("Player")) // if collided with gameobject with tag "Player"
         {
-            for(int i = 0; i < inventory.slot.Length; i++)
+            for(int i = 0; i < inventory.slot.Length; i++) 
+            // loop unless "i" value is more or equal inventory slot Length
+                //i is increase by 1 per loop
             {
-                if(inventory.isFull[i] == false)
+                if(inventory.isFull[i] == false) // if isFull[i] = false
                 {
-                    inventory.isFull[i] = true;
-                    Instantiate(itemButton, inventory.slot[i].transform);
-                    Destroy(gameObject);
-                    break;
+                    inventory.isFull[i] = true; // isFull[i] = true
+                    Instantiate(itemButton, inventory.slot[i].transform); // create prefab itemButton
+                    Destroy(gameObject); // destroy this gameobject
+                    break; // get out of loop
                 }
             }
         }
